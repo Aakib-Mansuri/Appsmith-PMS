7434240623dd5eab3a60e3642700a15cd7438bce
Aakib Mansuri <aakibmansuri958@gmail.com>
2025-10-14 06:29:57 +0000

    Add permission

--- pages/Wrokitem List/jsobjects/JSObject1/JSObject1.js
+++ pages/Wrokitem List/jsobjects/JSObject1/JSObject1.js
@@ -1,18 +1,18 @@
 export default {
-  async fetchReport() {
-    try {
-      const response = await Get_TeamDashboardData.run();
+	async fetchReport() {
+		try {
+			const response = await Get_TeamDashboardData.run();
 			showAlert("Report generated successfully!", "success");	
-			
+
 			const parsedData = response.map(r => ({
 				ResultSetName: r.ResultSetName,
 				Data: r.JsonData ? JSON.parse(r.JsonData) : []
 			}));
-			
+
 			storeValue("reportData", parsedData);
 			showModal("Report_Modal");
-    } catch (err) {
-      showAlert("Error generating report", "error");
-    }
-  }
+		} catch (err) {
+			showAlert("Error generating report", "error");
+		}
+	}
 }
